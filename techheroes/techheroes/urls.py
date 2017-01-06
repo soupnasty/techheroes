a592703fd7bb021a283dfeca60d44abc9cec8c72
Andrew Campbell <Eventup@Andrews-Macintosh.local>
2017-01-06 15:58:04 -0600

    setting up react

--- techheroes/techheroes/urls.py
+++ techheroes/techheroes/urls.py
@@ -1,6 +1,9 @@
+from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.decorators.cache import cache_page
 
+from . import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
@@ -8,5 +11,8 @@ urlpatterns = [
     url(r'^api/v1/heroes/', include('heroes.urls', namespace='heroes')),
     url(r'^api/v1/call-requests/', include('call_requests.urls', namespace='call_requests')),
     url(r'^api/v1/conferences/', include('conferences.urls', namespace='conferences')),
+
+     # catch all others because of how history is handled by react router - cache this page because it will never change
+    url(r'', cache_page(settings.PAGE_CACHE_SECONDS)(views.IndexView.as_view()), name='index'),
 
 ]
